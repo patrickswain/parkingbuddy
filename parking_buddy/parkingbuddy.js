39d34b068ebbaf9695999d08b89c88743e2b468f
patrickswain <patrickrswain@gmail.com>
2019-07-18 14:08:57 -0400

    adding routes

--- parking_buddy/parkingbuddy.js
+++ parking_buddy/parkingbuddy.js
@@ -14,6 +14,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/siteData', function (req, res) {
+  res.json('Site data reached!');
+})
+
 app.listen(3000, function () {
   //console.log('Example app listening on port 3000!');
 });
